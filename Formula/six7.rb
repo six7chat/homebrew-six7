44a2dcca43909ebf8d1a7053c2de3714290e8774
flavioaiello <11198273+flavioaiello@users.noreply.github.com>
2026-01-17 19:22:37 +0100

    chore: update korium to 24cf190

--- Formula/six7.rb
+++ Formula/six7.rb
@@ -7,7 +7,7 @@ class Six7 < Formula
   on_macos do
     on_arm do
       url "https://github.com/six7chat/homebrew-six7/releases/download/v0.7.6/six7-0.7.6-arm64-apple-darwin.tar.gz"
-      sha256 "013b596d6a5d523462c0b744d744635fe302d9ec43b7cce1c9d7d0a6d44877a7"
+      sha256 "6ae3f232d24ed054b8da6a6b3068380c1f9b79ee84e4fbd99a0cbf6cd2e1d848"
     end
   end
 
