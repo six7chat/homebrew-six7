ec2de424bf399b3ca3231ae47d4b4190c7f2934e
flavioaiello <11198273+flavioaiello@users.noreply.github.com>
2026-01-23 14:26:33 +0100

    Fix formula SHA256

--- Formula/six7.rb
+++ Formula/six7.rb
@@ -2,7 +2,7 @@ class Six7 < Formula
   desc "Secure peer-to-peer chatroom CLI built on Korium's adaptive networking fabric"
   homepage "https://github.com/six7chat/homebrew-six7"
   url "https://github.com/six7chat/homebrew-six7/archive/refs/tags/v0.6.7.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "b379b8697c4a73a7e7a2dcd5c9a90e4dcaea249f230a3b46510fbf7cc465f0aa"
   license "MIT"
   head "https://github.com/six7chat/homebrew-six7.git", branch: "main"
 
