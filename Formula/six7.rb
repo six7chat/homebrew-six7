8881d981043e55233384b2f20e16ebbdd2614f86
flavioaiello <11198273+flavioaiello@users.noreply.github.com>
2026-02-06 21:52:32 +0100

    Release v0.7.15

--- Formula/six7.rb
+++ Formula/six7.rb
@@ -1,13 +1,13 @@
 class Six7 < Formula
-  desc "Secure P2P chat CLI with Six7 Protocol v1.3 - powered by Korium 0.7.14"
+  desc "Secure P2P chat CLI with Six7 Protocol v1.3 - powered by Korium 0.7.15"
   homepage "https://github.com/six7chat/homebrew-six7"
-  version "0.7.14"
+  version "0.7.15"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/six7chat/homebrew-six7/releases/download/v0.7.14/six7-0.7.14-arm64-apple-darwin.tar.gz"
-      sha256 "1d14df32c844955a4cb44e9e61b286c52565a96c3fb518a267600e4ec9dd0e7d"
+      url "https://github.com/six7chat/homebrew-six7/releases/download/v0.7.15/six7-0.7.15-arm64-apple-darwin.tar.gz"
+      sha256 "6eefa7765164e57788219e3f43c1fcd1a2cd4fb85a2d4a9195544e8750838260"
     end
   end
 
