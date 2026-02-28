4bfcc909053ced428647a73bbfa78ddf93bef13c
flavioaiello <11198273+flavioaiello@users.noreply.github.com>
2026-02-28 22:51:42 +0100

    Upgrade to Korium v0.7.46

--- Formula/six7.rb
+++ Formula/six7.rb
@@ -1,13 +1,13 @@
 class Six7 < Formula
-  desc "Secure P2P chat CLI with Six7 Protocol v1.3 - powered by Korium 0.7.45"
+  desc "Secure P2P chat CLI with Six7 Protocol v1.3 - powered by Korium 0.7.46"
   homepage "https://github.com/six7chat/homebrew-six7"
-  version "0.7.45"
+  version "0.7.46"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/six7chat/homebrew-six7/releases/download/v0.7.45/six7-0.7.45-arm64-apple-darwin.tar.gz"
-      sha256 "c2235080d911c0c40ed26013f4d85789ccc975887d755caa0e8b64881e1830cc"
+      url "https://github.com/six7chat/homebrew-six7/releases/download/v0.7.46/six7-0.7.46-arm64-apple-darwin.tar.gz"
+      sha256 "59df72efe5e7222dbcd0b4f5f8edef623ee8f50fc5653b0056812de953d14137"
     end
   end
 
