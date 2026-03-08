4d4c37574d88172d06576167713c041ed3346c0e
flavioaiello <11198273+flavioaiello@users.noreply.github.com>
2026-03-08 11:30:36 +0100

    chore: bump to korium v0.7.66

--- Formula/six7.rb
+++ Formula/six7.rb
@@ -1,13 +1,13 @@
 class Six7 < Formula
-  desc "Secure P2P chat CLI with Six7 Protocol v1.3 - powered by Korium 0.7.65"
+  desc "Secure P2P chat CLI with Six7 Protocol v1.3 - powered by Korium 0.7.66"
   homepage "https://github.com/six7chat/homebrew-six7"
-  version "0.7.65"
+  version "0.7.66"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/six7chat/homebrew-six7/releases/download/v0.7.65/six7-0.7.65-arm64-apple-darwin.tar.gz"
-      sha256 "236c402eab5dc31fe1f840c81e43b1d2a2086834cf10f88e885602eee3715052"
+      url "https://github.com/six7chat/homebrew-six7/releases/download/v0.7.66/six7-0.7.66-arm64-apple-darwin.tar.gz"
+      sha256 "2fcd195617962cc470ab0170bfc760501afa546fa0eae154f11df59771714e10"
     end
   end
 
